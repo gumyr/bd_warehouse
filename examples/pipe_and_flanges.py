1b94dcb356ee52978eafa16d6583fec101646616
gumyr <gumyr9@gmail.com>
2023-06-13 14:17:48 -0400

    Adding pipes and flanges example

---
+++ examples/pipe_and_flanges.py
@@ -0,0 +1,23 @@
+from build123d import *
+from bd_warehouse.pipe import Pipe, PipeSection
+from bd_warehouse.flange import SlipOnFlange, WeldNeckFlange
+
+from ocp_vscode import show, show_object, set_port, set_defaults
+
+set_port(3939)
+set_defaults(reset_camera=False, ortho=True)
+
+
+inlet_flange = WeldNeckFlange(nps="12", flange_class=300, face_type="Ring")
+outlet_flange = SlipOnFlange(nps="12", flange_class=300)
+pipe = Pipe(
+    nps="12",
+    material="steel",
+    identifier="40",
+    path=Edge.make_line((0, 0, 0), (6 * FT, 0, 0)),
+)
+
+pipe.joints["inlet"].connect_to(inlet_flange.joints["pipe"])
+pipe.joints["outlet"].connect_to(outlet_flange.joints["pipe"])
+
+show(pipe, inlet_flange, outlet_flange)
